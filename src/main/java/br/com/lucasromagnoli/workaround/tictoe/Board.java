be25a1daecd7cf667083a3810aa9ed0de6f53a5f
Lucas Romagnoli <lucasr.romagnoli@gmail.com>
2023-01-22 02:30:56 -0300

    Atualizando construtor do board do tictoe

--- src/main/java/br/com/lucasromagnoli/workaround/tictoe/Board.java
+++ src/main/java/br/com/lucasromagnoli/workaround/tictoe/Board.java
@@ -4,25 +4,17 @@ import lombok.Setter;
 import lombok.ToString;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-import static br.com.lucasromagnoli.workaround.tictoe.Position.*;
+import static br.com.lucasromagnoli.workaround.tictoe.Position.values;
 
 @Setter
 @ToString
 public class Board {
-    private final Map<Position, Tile> tiles = new LinkedHashMap<>();
-
-    public Board() {
-        this.tiles.put(TOP_LEFT, new Tile(TOP_LEFT));
-        this.tiles.put(TOP_CENTER, new Tile(TOP_CENTER));
-        this.tiles.put(TOP_RIGHT, new Tile(TOP_RIGHT));
-        this.tiles.put(MIDDLE_LEFT, new Tile(MIDDLE_LEFT));
-        this.tiles.put(MIDDLE_CENTER, new Tile(MIDDLE_CENTER));
-        this.tiles.put(MIDDLE_RIGHT, new Tile(MIDDLE_RIGHT));
-        this.tiles.put(BOTTOM_LEFT, new Tile(BOTTOM_LEFT));
-        this.tiles.put(BOTTOM_CENTER, new Tile(BOTTOM_CENTER));
-        this.tiles.put(BOTTOM_RIGHT, new Tile(BOTTOM_RIGHT));
-    }
+    private final Map<Position, Tile> tiles = Arrays.stream(values())
+            .map(Tile::new)
+            .collect(Collectors.toMap(Tile::getPosition, Function.identity()));
 
     public Set<Tile> tiles() {
         return new LinkedHashSet<>(this.tiles.values());
